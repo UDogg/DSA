915b3242b9180aec7e83f20da1d9ed7992dc086c
Utkarsh Choudhary <ufc5009@psu.edu>
2023-07-19 23:54:29 +0530

    Create BFS.cpp

---
+++ Algorithms/BFS.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+void BFS(const Graph& graph, int start) {
+    vector<bool> visited(graph.V, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+        cout << current << " ";
+
+        for (int neighbor : graph.adjList[current]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int V = 5;
+    Graph g(V);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    cout << "BFS Traversal: ";
+    BFS(g, 0);
+    cout << endl;
+
+    return 0;
+}
