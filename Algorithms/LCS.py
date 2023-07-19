4db6b9f63b8d06a1d53d722f9bceebb0e9b5381f
Utkarsh Choudhary <ufc5009@psu.edu>
2023-07-19 23:41:41 +0530

    Create LCS.py

---
+++ Algorithms/LCS.py
@@ -0,0 +1,15 @@
+#Longest Common Subsequence
+#Finding the longest subsequence that is present in two (or more) sequences.
+# Pseudocode for LCS using dynamic programming
+def lcs(X, Y):
+    m, n = len(X), len(Y)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[m][n]
